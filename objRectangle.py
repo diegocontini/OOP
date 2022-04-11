1bd9b9ac9f408a1fd7502871fbfaf35d32837727
maddicone <75838650+maddicone@users.noreply.github.com>
2022-04-10 23:03:31 -0300

    Update objRectangle.py
    Done

--- objRectangle.py
+++ objRectangle.py
@@ -3,20 +3,55 @@ Atributos: LadoA, LadoB (ou Comprimento e Largura, ou Base e Altura, a escolher)
 Métodos: Mudar valor dos lados, Retornar valor dos lados, calcular Área e calcular Perímetro;
 '''
 class rectangle():
-    def __init__(self,base,altura,proprietario,serialNumber,lote):
+    def __init__(self,base,altura,proprietario,lote, quantidade): 
         self.base = base
         self.altura = altura
         self.proprietario = proprietario
-        self.serialNumber = serialNumber
         self.lote = lote
+        self.quantidade = quantidade
+
+    #Metodo para mudar o valor dos lados
     def alterarBaseAltura(self,):
-        print(f'O valor atual da base é {self.base}\nO valor atual da altura é: {self.altura} ')
+        print(f'O valor atual da base é: {self.base}\nO valor atual da altura é: {self.altura} ')
         nBase = int(input('Digite um novo valor inteiro para a base: '))
         nAltura = int(input('Digite um novo valor inteiro para a altura: '))
         self.base = nBase
         self.altura = nAltura 
-        print(self.base,self.altura)
 
-rectangle1 = rectangle(10,20,'Diego','2022/0001','2022/04/09',)
-rectangle1.alterarBaseAltura()       
+    #Metodo para retornar o valor dos lados
+    def verBaseAltura(self):
+        print(f'O valor atual da base é: {self.base}\nO valor atual da altura é: {self.altura}')
+    
+    #Metodo para calcular a área
+    def areaRectangle(self):
+        rectangleArea = self.base * self.altura
+        #print(f'A área do retângulo é: {rectangleArea}.')
+
+    #Metodo para calcular o perímetro
+    def perimetroRectangle(self):
+        rectanglePerimetro = 2 * (self.base + self.altura)
+        #print(f'O perímetro do retângulo é: {rectanglePerimetro}')
+
+    #Metodo para trocar o proprietário
+    def alterarProprietario(self):
+        newProprietario = str(input('Digite a razão social do novo proprietário do retângulo: '))
+        self.proprietario =  newProprietario
+        print(f'A posse do retângulo passou para {self.proprietario}')
+
+    #Metodo para atualizar o estoque do produto
+    def contagemEstoque(self):
+        saldoEstoque = int(input('Você iniciou a contagem de estoque do produto, digite o novo valor em estoque do item:\n'))
+        self.quantidade = saldoEstoque
+        print(f'O estoque atual do produto é: {self.quantidade}')
+
+
+rectangle1 = rectangle(10,20,'Diego','2022/0001',25)
+rectangle1.areaRectangle()
+rectangle1.alterarBaseAltura()
+rectangle1.perimetroRectangle()
+rectangle1.alterarProprietario()
+rectangle1.contagemEstoque()
+
+
+
         
